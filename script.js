7b03408de0bbcb2cccd0d78ed803f1ed21b701b3
rajanhere <rajan@brainstackhub.com.au>
2024-01-23 21:39:01 +1100

    vanilla js calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// const button= document.getElementById("btn");
+
+// const changeBg= function(){
+//   const colors=["violet","green","blue","yellow","purple","maroon","Grey","pink", "Aquamarine", "crimson", "bridge"];
+  
+//   const randomNumber=(Math.floor(Math.random()*colors.length));
+//   const randomColor=colors[randomNumber];
+
+
+//   document.body.style.background=randomColor;
+//   console.log(randomNumber);
+// console.log(randomColor);
+// };
+// button.addEventListener("click", changeBg);
+
+
